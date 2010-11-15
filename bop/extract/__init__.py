daa285b6bc0dc03c609f80177699087799be19e2
microdan <microdan@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2010-11-15 05:00:28 +0000

    Command-line options for toggling between different policy classes

--- bop/extract/__init__.py
+++ bop/extract/__init__.py
@@ -22,27 +22,26 @@ def buildLibrary(path, scaler, featureExtractor):
 
 
 class Scaler(object):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self, dimension):
+        self.dimension = dimension
     def scale(self, image):
-        scaled = cv.CreateImage((self.width, self.height), 8, 1)
+        scaled = cv.CreateImage((self.dimension, self.dimension), 8, 1)
         cv.Resize(image, scaled, cv.CV_INTER_NN)
         return scaled
 
 class ProportionalScaler(Scaler):
     def __init__(self, dimension):
-        Scaler.__init__(self, dimension, dimension)
+        Scaler.__init__(self, dimension)
     def scale(self, image):
-        scaled = cv.CreateImage((self.width, self.height), 8, 1)
+        scaled = cv.CreateImage((self.dimension, self.dimension), 8, 1)
         if image.width > image.height:
-            factor = float(self.width) / image.width
-            targetWidth = self.width
+            factor = float(self.dimension) / image.width
+            targetWidth = self.dimension
             targetHeight = image.height*factor
         else:
-            factor = float(self.height) / image.height
+            factor = float(self.dimension) / image.height
             targetWidth = image.width*factor
-            targetHeight = self.height
+            targetHeight = self.dimension
         matrix = cv.CreateMat(2, 3, cv.CV_64FC1)
         cv.GetAffineTransform([(0, 0), (image.height, 0), (0, image.width)], [(0, 0), (targetHeight, 0), (0, targetWidth)], matrix)
         #print "The transform is", matrix
