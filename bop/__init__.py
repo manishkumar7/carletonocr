8b9d900ee9f53ffbb944c632bfbd61ab26d2026a
microdan <microdan@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2010-11-16 17:14:21 +0000

    Command-line options to save intermediate stages

--- bop/__init__.py
+++ bop/__init__.py
@@ -1,4 +1,6 @@
 __all__ = ["binarize", "extract", "match", "segment", "typeset", "linguistics", "OCR"]
+import cv
+import os
 
 class OCR:
     def __init__(self, image, binarizer, segmenter, typesetter, matcher, linguist):
@@ -9,10 +11,25 @@ class OCR:
         self.matcher = matcher
         self.linguist = linguist
         
-    def recognize(self):
+    def recognize(self, saveBinarized, saveSegmented, saveTypeset, saveMatcher):
         blackAndWhite = self.binarizer.binarize(self.image)
+        if saveBinarized != None:
+            cv.SaveImage(saveBinarized, blackAndWhite)
         characterPieces = self.segmenter.segment(blackAndWhite)
+        if saveSegmented != None:
+            os.mkdir(saveSegmented)
+            for i in range(len(characterPieces)):
+                cv.SaveImage(saveSegmented+"/"+str(i)+".png", characterPieces[i][1])
         pieces = self.typesetter.typeset(characterPieces)
-        output = self.matcher.match(pieces)
-        output = self.linguist.correct(output)
+        if saveTypeset != None:
+            os.mkdir(saveTypeset)
+            for i in range(len(pieces)):
+                if not isinstance(pieces[i], str):
+                    cv.SaveImage(saveTypeset+"/"+str(i)+".png", pieces[i])
+        possibilities = self.matcher.match(pieces)
+        if saveMatcher != None:
+            matcherOutput = file(saveMatcher, "w")
+            matcherOutput.write(str(possibilities))
+            matcherOutput.close()
+        output = self.linguist.correct(possibilities)
         return output
