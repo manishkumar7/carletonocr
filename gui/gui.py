d8aaa367a4f988640558a12321a40bfbf9c21e3f
brenneraeric <brenneraeric@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2011-01-08 23:43:30 +0000

    Adding more GUI options

--- gui/gui.py
+++ gui/gui.py
@@ -3,56 +3,104 @@ from notebook import notebook
 import tkFileDialog
 
 class OCRWindow:
-    def __init__(self):
-		self.application = Tk()
-		self.application.geometry("600x400+0+0")
-		self.tabs = notebook(self.application, TOP)
-		
-		# uses the notebook's frame
-		self.original = Frame(self.tabs())
-		imageFrame = Frame(self.original, borderwidth=1, relief=RIDGE)
-		self.image = Label(imageFrame, text="No image loaded")
-		self.image.pack(fill=X)
-		imageFrame.pack(side=LEFT, fill=X)
-		Frame(self.original, width=2, background="#000000").pack(side=LEFT, fill=BOTH)
-		originalOptionsFrame = Frame(self.original)
-		fileChooser = Button(originalOptionsFrame, text="Choose file", command=self.chooseFile)
-		fileChooser.pack(side=TOP)
-		self.fileLabel = Label(originalOptionsFrame)
-		self.fileLabel.pack(side=TOP)
-		originalOptionsFrame.pack(side=LEFT, fill=X)
-		
-		self.binary = Frame(self.tabs())
-		self.segmented = Frame(self.tabs())
-		self.features = Frame(self.tabs())
-		self.matched = Frame(self.tabs())
-		self.output = Frame(self.tabs())
-		
-		# keeps the reference to the radiobutton (optional)
-		self.tabs.add_screen(self.original, "Original")
-		self.tabs.add_screen(self.binary, "Binary")
-		self.tabs.add_screen(self.segmented, "Segmented")
-		self.tabs.add_screen(self.features, "Features")
-		self.tabs.add_screen(self.matched, "Matched")
-		self.tabs.add_screen(self.output, "Final output")
-		
-		self.filename = None
-		self.update()
-		
-    def run(self):
-    	self.application.mainloop()
+    def addTab(self, imageName, optionFunc):
+        tab = Frame(self.tabs())
+        imageFrame = Frame(tab)
+        imageLabel = Label(imageFrame, text="No image loaded")
+        imageLabel.pack(fill=X)
+        setattr(self, imageName, imageLabel)
+        imageFrame.pack(side=LEFT, fill=X)
+        Frame(tab, width=2, background="#000000").pack(side=LEFT, fill=BOTH)
+        optionsFrame = Frame(tab)
+        optionFunc(optionsFrame)
+        optionsFrame.pack(side=LEFT, fill=X)
+        return tab
+    
+    def fileOptions(self, frame):
+        fileChooser = Button(frame, text="Choose file", command=self.chooseFile)
+        fileChooser.pack(side=TOP)
+        self.fileLabel = Label(frame)
+        self.fileLabel.pack(side=TOP)
+    
+    def dropdown(self, frame, name, attr, options):
+    	innerFrame = Frame(frame)
+    	Label(innerFrame, text=name+": ").pack(side=LEFT)
+    	var = StringVar()
+    	var.set(options[0])
+    	var.trace('w', self.somethingChanged)
+    	OptionMenu(innerFrame, var, *options).pack(side=LEFT)
+    	setattr(self, attr, var)
+    	innerFrame.pack(side=TOP)
+    
+    def entry(self, frame, name, attr, default):
+        innerFrame = Frame(frame)
+    	Label(innerFrame, text=name+": ").pack(side=LEFT)
+    	var = StringVar()
+    	var.set(default)
+    	var.trace('w', self.somethingChanged)
+    	Entry(innerFrame, textvariable=var).pack(side=LEFT)
+    	setattr(self, attr, var)
+    	innerFrame.pack(side=TOP)
+    
+    def binarizerOptions(self, frame):
+        self.dropdown(frame, 'Binarizer', 'binarizer', ['global binarizer', 'adaptive binarizer'])
+    
+    def segmenterOptions(self, frame):
+        self.dropdown(frame, 'Segmenter', 'segmenter', ['connected components', 'bounding box'])
     	
+    def typesetterOptions(self, frame):
+        self.dropdown(frame, 'Typesetter', 'typesetter', ["linear typesetter", "no typesetter"])
+        self.entry(frame, 'Width of space', 'spaceWidth', '.25')
+
+    def featureExtractorOptions(self, frame):
+        self.dropdown(frame, 'Feature extractor', 'featureExtractor', [ "template", "old template", "histogram", "vertical histogram", "horizontal histogram"])
+
+    def matcherOptions(self, frame):
+        self.entry(frame, 'k nearest neighbors', 'k', '1')
+
+    def linguistOptions(self, frame):
+        self.dropdown(frame, 'Linguist', 'linguist', ["n-gram", "no linguist"])
+
+    def __init__(self):
+        self.application = Tk()
+        #self.application.geometry("600x400+0+0")
+        self.tabs = notebook(self.application, TOP)
+        
+        tabParameters = [
+            ('image', self.fileOptions, "Original"),
+            ('binarized', self.binarizerOptions, "Binary"),
+            ('segmented', self.segmenterOptions, "Segmented"),
+            ('typesetter', self.typesetterOptions, "Typeset"),
+            ('features', self.featureExtractorOptions, "Features"),
+            ('matched', self.matcherOptions, "Matched"),
+            ('output', self.linguistOptions, "Final output")
+        ]
+        
+        tabs = [(self.addTab(attr, options), name) for attr, options, name in tabParameters]
+        for tab, name in tabs:
+        	self.tabs.add_screen(tab, name)
+        
+        self.filename = None
+        self.update()
+        
+    def run(self):
+        self.application.mainloop()
+        
     def chooseFile(self):
-    	self.filename = tkFileDialog.askopenfilename()
-    	self.update()
+        self.filename = tkFileDialog.askopenfilename()
+        self.update()
+    
+    def somethingChanged(self, *args): #What are the args?
+        self.update()
     
     def update(self):
-    	self.fileLabel.config(text=self.filename if self.filename is not None else "No file selected")
-    	if self.filename:
-    		photo = PhotoImage(file=self.filename)
-    		self.image.config(image=photo, text=None)
-    		self.image.photo = photo
-    	
+        self.fileLabel.config(text=self.filename if self.filename is not None else "No file selected")
+        if self.filename:
+            #TODO: Actually use ImageTk in PyPIL so that we don't just support .gif
+            photo = PhotoImage(file=self.filename)
+            self.image.config(image=photo, text=None)
+            self.image.photo = photo
+        
 if __name__ == "__main__":
-	gui = OCRWindow()
-	gui.run()
+    gui = OCRWindow()
+    gui.run()
