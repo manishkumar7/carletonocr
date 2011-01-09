3d3264265ab8207aeee54d0004cf26daa7ed9abd
mccartya@carleton.edu <mccartya@carleton.edu@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2011-01-09 23:52:08 +0000

    Added first version of adaptive binarizer and updated command line arguments

--- trunk/ocr/binarize/binarize.py
+++ trunk/ocr/binarize/binarize.py
@@ -5,17 +5,15 @@ class Binarizer:
     #Thresholding (how do we get the threshold?)
     #Locally adaptive binarization
     def binarize(self, image):
-        '''Given an image, return a black and white image, together with
-        a probability that this is correct'''
+        '''Given an image, return a black and white image'''
         #Potential pain point in the future:
         #What if we want to use a grayscale algorithm?
         return image
         
 class SimpleBinarizer(Binarizer):
     def binarize(self, im):
-        '''Given an image, return a black and white image, together with
-        a probability that it is correct. Uses OPenCV's adaptive
-        thresholding with blockSize as large as possible'''
+        '''Given an image, return a black and white image. Uses OPenCV's 
+        adaptive thresholding with blockSize as large as possible'''
         #create an image that will eventually be a binarization of the input image
         thresh = cv.CreateImage((im.width, im.height), 8, 1)
         #get parameter values
@@ -33,3 +31,39 @@ class SimpleBinarizer(Binarizer):
                 blockSize = blockSize - 1
         return blockSize
 
+class LocalBinarizer(Binarizer):
+    def binarize(self, im):
+        '''Given an image, return a black and white image. Uses OPenCV's 
+        adaptive thresholding with blockSize as large as possible'''
+        #create an image that will eventually be a binarization of the input image
+        thresh = [cv.CreateImage((im.width, im.height), 8, 1) for i in range(3)]
+        #get parameter values
+        maxVal = 255
+        bSize = self.getBlockSize(im)
+        #create the binarized image
+        bestThreshold, numWhitePixels, bestPixels = 0
+        channels = [[] for i in range(3)]
+        cv.split(im, channels)
+        for threshold, channel in thresh, channels:
+        	cv.AdaptiveThreshold(channel, threshold, maxVal, blockSize=bSize)
+        for threshold in thresh:
+        	for row in threshold.height:
+        		for col in threshold.width:
+        			if threshold[row,col] == 1:
+        				numWhitePixels += 1
+        	if numWhitePixels > bestPixels:
+        		bestPixels = numWhitePixels
+        		bestThreshold = threshold
+        	numWhitePixels = 0
+        return bestThreshold
+        
+    def getBlockSize(self, image, proportion=1):
+        '''Given an image, determines the blockSize argument for binarize().
+        Defaults to the largest possible blockSize, which can be scaled down.'''
+        if proportion > 1:
+        	blockSize = min(image.width, image.height)
+        else:
+        	blockSize = int(min(image.width, image.height) * proportion)
+        if blockSize % 2 == 0:
+                blockSize = blockSize - 1
+        return blockSize
