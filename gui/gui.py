c569c0518f7c315aec04842ca250b95157b635a4
microdan <microdan@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2011-01-12 23:01:41 +0000

    Rewriting GUI in wxWidgets because MacPorts is dumb

--- gui/gui.py
+++ gui/gui.py
@@ -1,77 +1,100 @@
-from Tkinter import *
-from notebook import notebook
-import tkFileDialog
 import cv
+import wx
+import wx.aui
+import os
 
-def tkImageFromCv(image):
+def wxImageFromCv(image):
     path = "/tmp/image.gif"
     cv.SaveImage(path, image)
-    return PhotoImage(file=path)
+    return wx.Image(path)
 
-class OCRWindow:
-    def addTab(self, imageName, optionFunc):
-        tab = Frame(self.tabs())
-        imageFrame = Frame(tab)
-        imageLabel = Label(imageFrame, text="No image loaded")
-        imageLabel.pack(fill=X)
-        setattr(self, imageName, imageLabel)
-        imageFrame.pack(side=LEFT, fill=X)
-        Frame(tab, width=2, background="#000000").pack(side=LEFT, fill=BOTH)
-        optionsFrame = Frame(tab)
-        optionFunc(optionsFrame)
-        optionsFrame.pack(side=LEFT, fill=X)
-        return tab
+class TabWindow():
+
+    def beside(self, parent, sizer, contents):
+        panel = wx.Panel(parent=parent)
+        left, right = contents(panel)
+        innerSizer = wx.BoxSizer(wx.HORIZONTAL)
+        innerSizer.Add(left, 1, wx.EXPAND)
+        innerSizer.Add(right, 1, wx.EXPAND)
+        panel.SetSizer(innerSizer)
+        if sizer is not None: sizer.Add(panel, 0, wx.EXPAND)
+        return panel
+
+    def tab(self, notebook, attr, optionsFunc):
+        def contents(panel):
+            textPanel = wx.Panel(parent=panel, style=wx.SUNKEN_BORDER)
+            setattr(self, attr, textPanel)
+            text = wx.StaticText(parent=textPanel, label="No image loaded")
+            options = wx.Panel(parent=panel, style=wx.SUNKEN_BORDER)
+            optionsSizer = wx.BoxSizer(wx.VERTICAL)
+            optionsFunc(options, optionsSizer)
+            options.SetSizer(optionsSizer)
+            return textPanel, options
+        return self.beside(notebook, None, contents)
+
+    def fileOptions(self, parent, sizer):
+        def contents(panel):
+            button = wx.Button(panel, wx.ID_ANY, "Choose file")
+            name = wx.StaticText(panel, label="No file loaded")
+            def chooseFile(evt):
+                dialog = wx.FileDialog(parent, message="Open an Image", defaultDir=os.getcwd(), style=wx.OPEN)
+                if dialog.ShowModal() == wx.ID_OK:
+                    self.filename = dialog.GetPath()
+                    self.update()
+                dialog.Destroy()
+            self.app.Bind(wx.EVT_BUTTON, chooseFile, button)
+            return button, name
+        self.beside(parent, sizer, contents)
+
+    def besideLabel(self, parent, sizer, name, right):
+        def contents(panel):
+            label = wx.StaticText(panel, label=name+": ")
+            control = right(panel)
+            return label, control
+        self.beside(parent, sizer, contents)
+
+    def dropdown(self, parent, sizer, name, attr, options):
+        def contents(panel):
+             control = wx.ComboBox(panel, value=options[0], choices=options, style=wx.CB_DROPDOWN)
+             def changeOption(*args):
+                 setattr(self, attr, control.GetValue())
+                 self.update()
+             self.app.Bind(wx.EVT_COMBOBOX, changeOption, control)
+             return control
+        self.besideLabel(parent, sizer, name, contents)
+
+    def entry(self, parent, sizer, name, attr, default):
+        def contents(panel):
+            entry = wx.TextCtrl(panel)
+            def changeText(*args):
+                setattr(self, attr, entry.GetValue())
+                self.update()
+            self.app.Bind(wx.EVT_TEXT_ENTER, changeText, entry)
+            return entry
+        self.besideLabel(parent, sizer, name, contents)
+
+    def binarizerOptions(self, frame, sizer):
+        self.dropdown(frame, sizer, 'Binarizer', 'binarizer', ['global binarizer', 'adaptive binarizer'])
     
-    def fileOptions(self, frame):
-        fileChooser = Button(frame, text="Choose file", command=self.chooseFile)
-        fileChooser.pack(side=TOP)
-        self.fileLabel = Label(frame)
-        self.fileLabel.pack(side=TOP)
-    
-    def dropdown(self, frame, name, attr, options):
-    	innerFrame = Frame(frame)
-    	Label(innerFrame, text=name+": ").pack(side=LEFT)
-    	var = StringVar()
-    	var.set(options[0])
-    	var.trace('w', self.somethingChanged)
-    	OptionMenu(innerFrame, var, *options).pack(side=LEFT)
-    	setattr(self, attr, var)
-    	innerFrame.pack(side=TOP)
-    
-    def entry(self, frame, name, attr, default):
-        innerFrame = Frame(frame)
-    	Label(innerFrame, text=name+": ").pack(side=LEFT)
-    	var = StringVar()
-    	var.set(default)
-    	var.trace('w', self.somethingChanged)
-    	Entry(innerFrame, textvariable=var).pack(side=LEFT)
-    	setattr(self, attr, var)
-    	innerFrame.pack(side=TOP)
-    
-    def binarizerOptions(self, frame):
-        self.dropdown(frame, 'Binarizer', 'binarizer', ['global binarizer', 'adaptive binarizer'])
-    
-    def segmenterOptions(self, frame):
-        self.dropdown(frame, 'Segmenter', 'segmenter', ['connected components', 'bounding box'])
+    def segmenterOptions(self, frame, sizer):
+        self.dropdown(frame, sizer, 'Segmenter', 'segmenter', ['connected components', 'bounding box'])
     	
-    def typesetterOptions(self, frame):
-        self.dropdown(frame, 'Typesetter', 'typesetter', ["linear typesetter", "no typesetter"])
-        self.entry(frame, 'Width of space', 'spaceWidth', '.25')
+    def typesetterOptions(self, frame, sizer):
+        self.dropdown(frame, sizer, 'Typesetter', 'typesetter', ["linear typesetter", "no typesetter"])
+        self.entry(frame, sizer, 'Width of space', 'spaceWidth', '.25')
 
-    def featureExtractorOptions(self, frame):
-        self.dropdown(frame, 'Feature extractor', 'featureExtractor', [ "template", "old template", "histogram", "vertical histogram", "horizontal histogram"])
+    def featureExtractorOptions(self, frame, sizer):
+        self.dropdown(frame, sizer, 'Feature extractor', 'featureExtractor', [ "template", "old template", "histogram", "vertical histogram", "horizontal histogram"])
 
-    def matcherOptions(self, frame):
-        self.entry(frame, 'k nearest neighbors', 'k', '1')
+    def matcherOptions(self, frame, sizer):
+        self.entry(frame, sizer, 'k nearest neighbors', 'k', '1')
 
-    def linguistOptions(self, frame):
-        self.dropdown(frame, 'Linguist', 'linguist', ["n-gram", "no linguist"])
-
-    def __init__(self):
-        self.application = Tk()
-        #self.application.geometry("600x400+0+0")
-        self.tabs = notebook(self.application, TOP)
-        
+    def linguistOptions(self, frame, sizer):
+        self.dropdown(frame, sizer, 'Linguist', 'linguist', ["n-gram", "no linguist"])
+    
+    def tabSet(self, frame):
+        panel = wx.Panel(parent=frame)
+        notebook = wx.aui.AuiNotebook(panel)
         tabParameters = [
             ('image', self.fileOptions, "Original"),
             ('binarized', self.binarizerOptions, "Binary"),
@@ -82,30 +105,25 @@ class OCRWindow:
             ('output', self.linguistOptions, "Final output")
         ]
         
-        tabs = [(self.addTab(attr, options), name) for attr, options, name in tabParameters]
-        for tab, name in tabs:
-        	self.tabs.add_screen(tab, name)
-        
+        for attr, options, name in tabParameters:
+            notebook.AddPage(self.tab(notebook, attr, options), name)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(notebook, 1, wx.EXPAND)
+        panel.SetSizer(sizer)
+    
+    def __init__(self):
         self.filename = None
-        self.update()
-        
-    def run(self):
-        self.application.mainloop()
-        
-    def chooseFile(self):
-        self.filename = tkFileDialog.askopenfilename()
-        self.update()
-    
-    def somethingChanged(self, *args): #What are the args?
-        self.update()
-    
+        self.app = wx.PySimpleApp()
+        frame = wx.Frame(None, title="Optical Character Recognition", size=(600, 400))
+        self.tabSet(frame)
+        frame.Show()
+        self.app.MainLoop()
+
     def update(self):
-        self.fileLabel.config(text=self.filename if self.filename is not None else "No file selected")
-        if self.filename:
-            photo = tkImageFromCv(cv.LoadImage(self.filename))
-            self.image.config(image=photo, text=None)
-            self.image.photo = photo
-        
-if __name__ == "__main__":
-    gui = OCRWindow()
-    gui.run()
+        if self.filename is None:
+            print "No file selected"
+        else:
+            print "The file name is", self.filename
+
+if __name__ == '__main__':
+    TabWindow()
