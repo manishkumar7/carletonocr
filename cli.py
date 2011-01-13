eda35734ba6dafa27318ad8c52354ce4ed6f761d
microdan <microdan@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2011-01-13 20:18:02 +0000

    Further refactoring of CLI

--- cli.py
+++ cli.py
@@ -14,43 +14,30 @@ def getOptions():
     Returns a parser object.
     """
 
+    def addOption(name, attr, help, type="string"):
+        parser.add_option(name, action="store", dest=attr, default=getattr(defaultOptions, attr), help=help, type=type)
+
+    def addClassOption(name, attr, desc):
+        addOption(name, attr, help="%s. Options: %s. Default: %s" % (desc, ', '.join(classMap[attr].keys()), getattr(defaultOptions, attr)))
+
     parser = optparse.OptionParser(usage="usage: %prog [options] image", version="%prog 0.1")
 
-    parser.add_option("--space-width", action="store", type="float", dest="spaceWidth", default=defaultOptions.spaceWidth,
-            help="What proportion of the average character width is the width of a space")
+    addOption('--space-width', 'spaceWidth', type='float', help="What proportion of the average character width is the width of a space")
+    addOption('--library-dir', 'library', help="Library directory for the Matcher. This should be a directory containing subdirectories, within which are PNG files named for the character they represent. allfont.py in util can generate these on machines with freetype and PIL.")
+    addOption('--dimension', 'dimension', type='int', help="Dimension of square scaled image within the template matcher")
+    addOption('-k', 'k', type='int', help="How many templates should vote on which character is chosen. k=1 means the single most similar template determines the character.")
 
-    parser.add_option("-l", "--library-dir", action="store", dest="library",
-            default=defaultOptions.library,
-            help="Library directory for the Matcher. This should be a directory containing subdirectories, within which are PNG files named for the character they represent. allfont.py in util can generate these on machines with freetype and PIL.")
+    addClassOption('--binarizer', 'binarizer', "Binarizer policy")
+    addClassOption('--segmenter', 'segmenter', "Segmentation policy")
+    addClassOption('--typesetter', 'typesetter', "Typesetting policy")
+    addClassOption('--feature-extractor', 'featureExtractor', "Feature extraction policy")
+    addClassOption('--scaler', 'scaler', "Scaling policy")
+    addClassOption('--linguist', 'linguist', "Linguistic correction policy")
 
-    parser.add_option("-d", "--dimension", action="store", type="int", dest="dimension", default=defaultOptions.dimension,
-            help="Dimension of square scaled image within the template matcher")
-
-    parser.add_option("-k", action="store", type="int", dest="k", default=defaultOptions.k,
-            help="How many templates should vote on which character is chosen. k=1 means the single most similar template determines the character.")
-
-    def addOption(name, attr, desc):
-        parser.add_option(name, action="store", dest=attr, default=getattr(defaultOptions, attr),
-            help = "%s. Options: %s. Default: %s" % (desc, ', '.join(classMap[attr].keys()), getattr(defaultOptions, attr)))
-
-    addOption('--binarizer', 'binarizer', "Binarizer policy")
-    addOption('--segmenter', 'segmenter', "Segmentation policy")
-    addOption('--typesetter', 'typesetter', "Typesetting policy")
-    addOption('--feature-extractor', 'featureExtractor', "Feature extraction policy")
-    addOption('--scaler', 'scaler', "Scaling policy")
-    addOption('--linguist', 'linguist', "Linguistic correction policy")
-
-    parser.add_option("--save-binarized", action="store", dest="saveBinarized", default=defaultOptions.saveBinarized,
-            help="Set this to a path to save the binarized image to a particular location")
-
-    parser.add_option("--save-segmented", action="store", dest="saveSegmented", default=defaultOptions.saveSegmented,
-            help="Set this to a directory to save the segmented images in")
-
-    parser.add_option("--save-typeset", action="store", dest="saveTypeset", default=defaultOptions.saveTypeset,
-            help="Set this to a directory to save the output of the typesetter")
-
-    parser.add_option("--save-matcher", action="store", dest="saveMatcher", default=defaultOptions.saveMatcher,
-            help="Set this to a path to save the matcher's results as")
+    addOption('--save-binarized', 'saveBinarized', 'Set this to a path to save the binarized image to a particular location')
+    addOption('--save-segmented', 'saveSegmented', 'Set this to a path to save the segmented image to a particular location')
+    addOption('--save-typeset', 'saveTypeset', 'Set this to a path to save the segmented image to a particular location')
+    addOption('--save-matcher', 'saveMatcher', 'Set this to a path to save the matched image to a particular location')
 
     (options, args) = parser.parse_args()
 
