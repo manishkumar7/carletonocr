fb537d3f81d3be87387f470efaac21a9706134d4
palmcarp <palmcarp@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2011-02-01 06:15:24 +0000

    Library is generated from fonts on-the-fly. This makes loading faster and makes the program cleaner, easier-to-use

--- trunk/ocr/extract/library.py
+++ trunk/ocr/extract/library.py
@@ -1,21 +1,39 @@
 import cv
 import os
 from ocr.typeset import characterCombine
+import Image, ImageFont, ImageDraw, ImageChops
 
-def buildLibrary(path):
-    dirs = os.listdir(path)
+fonts = ["Times New Roman", "Arial", "Courier New", "Georgia", "Verdana", "Tahoma"]
+pointSize = 100
+charsToGenerate = map(chr, range(ord('!'), ord('~')+1))
+
+def trim(im, border):
+    bg = Image.new(im.mode, im.size, border)
+    diff = ImageChops.difference(im, bg)
+    diff.save("lib2.png")
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+    else:
+        # found no content
+        raise ValueError("cannot trim; image was empty")
+
+def render(font, char):
+    font = ImageFont.truetype('/Library/Fonts/'+font+'.ttf', pointSize)
+    im = Image.new("L", (20, 20), 255)
+    draw = ImageDraw.Draw(im)
+    size = draw.textsize(char, font=font)
+    im = im.resize(size)
+    draw = ImageDraw.Draw(im)
+    draw.text((0,0), char, font=font)
+    im = trim(im, 255)
+    cv_im = cv.CreateImageHeader(im.size, cv.IPL_DEPTH_8U, 1)
+    cv.SetData(cv_im, im.tostring())
+    return cv_im
+
+def buildLibrary(path): #ignore path
     chars = []
-    for dir in dirs:
-    	place = path+os.sep+dir
-        try:
-            files = os.listdir(place)
-            for file in files:
-                #print file
-                im = cv.LoadImage(place + os.sep + file, cv.CV_LOAD_IMAGE_COLOR)
-                chars.append((file[0], im)) 
-                #processed = characterCombine(typesetter.typeset(processed))
-                #cv.SaveImage("/Accounts/ehrenbed/Desktop/stuff2/"+file, processed)
-        except OSError:
-            # Goddamn OS X and its goddamn DS_STORE crap
-            pass
+    for font in fonts:
+        for char in charsToGenerate:
+            chars.append((char, render(font, char)))
     return chars
