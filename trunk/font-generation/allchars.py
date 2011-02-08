efd49982a4c7c90392b4c56afa51885c69c3c2e0
microdan <microdan@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2011-02-08 04:04:49 +0000

    Removing unused static font generation

--- trunk/font-generation/allchars.py
+++
@@ -1,2 +0,0 @@
-import string
-for s in string.printable[:94]: print s
--- trunk/font-generation/quick.py
+++
@@ -1,8 +0,0 @@
-import cv
-import sys
-
-win_name = 'win'
-im = cv.LoadImage(sys.argv[1])
-cv.ShowImage(win_name, im)
-
-input("")
