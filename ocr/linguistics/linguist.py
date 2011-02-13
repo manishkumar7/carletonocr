2b666b71651c90fcc483444b55af79bccbe67727
palmcarp <palmcarp@b32c9fa4-4b31-d35f-c426-6f00eafc69b3>
2011-02-13 08:16:46 +0000

    Final commit of the night - cleaned things up and tuned parameters well.

--- ocr/linguistics/linguist.py
+++ ocr/linguistics/linguist.py
@@ -70,15 +70,6 @@ class NullLinguist(StreamLinguist):
 
     def modelProbability(self, character, context):
         return 1
-        
-"""class NullLinguist(Linguist):
-    def correct(self, characterPossibilities):
-        def transform(item):
-            if isinstance(item, str):
-                return item
-            else:
-                return sorted(item, key=lambda x: x[1], reverse=True)[0][0]
-        return map(transform, characterPossibilities)"""
 
 class NGramProgram(object):
     def __init__(self):
